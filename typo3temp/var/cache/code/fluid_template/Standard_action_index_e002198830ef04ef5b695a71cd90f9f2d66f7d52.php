66d64ecb0040a04f0c6fb45aa9e822def5b26162
Mesud Mujanovic <mesud.mujanovic@bitconex.de>
2024-05-19 23:34:31 +0200

    d

---
+++ typo3temp/var/cache/code/fluid_template/Standard_action_index_e002198830ef04ef5b695a71cd90f9f2d66f7d52.php
@@ -0,0 +1,75 @@
+<?php
+
+class Standard_action_index_e002198830ef04ef5b695a71cd90f9f2d66f7d52 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>mesud</h1>
+';
+// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+return NULL;
+};
+$arguments1 = array();
+$arguments1['section'] = NULL;
+$arguments1['partial'] = NULL;
+$arguments1['delegate'] = NULL;
+$arguments1['renderable'] = NULL;
+$arguments1['arguments'] = array (
+);
+$arguments1['optional'] = false;
+$arguments1['default'] = NULL;
+$arguments1['contentAs'] = NULL;
+$arguments1['debug'] = true;
+$arguments1['partial'] = 'Partials';
+
+$output0 .= TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper::renderStatic($arguments1, $renderChildrenClosure2, $renderingContext);
+
+$output0 .= '
+
+</body>
+</html>';
+
+return $output0;
+}
+
+
+}
+#
